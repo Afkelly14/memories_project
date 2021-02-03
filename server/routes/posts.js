5a459298f116f4618ae3cead41fbf97466505f83
Alex_Kelly <afkelly14@Gmail.com>
2021-02-02 20:48:33 -0500

    posts setup, adding front end styling

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/post.js';
+import { getPosts, createPosts } from '../controllers/posts.js';
 
 const router = express.Router();
 
 //https://localhost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPosts);
 
 export default router;
