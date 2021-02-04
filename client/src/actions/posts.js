404f16b985fdfdc2e20bcb6bb014d624a6da2b40
Alex_Kelly <afkelly14@Gmail.com>
2021-02-03 21:06:57 -0500

    adding form component

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,13 +3,13 @@ import * as api from '../api';
 // Action creators
 // reduxthunk allows you to specify another arrow function
 // functions that return actions
-const getPosts = () => async (dispatch) => {
+export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
         //pass data from the backend to disptach an action using redux
         dispatch({ type: 'FETCH_ALL', payload: data });
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
     }
 
    
--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://123:123@cluster0.hdibf.mongodb.net/<dbname>?retryWrites=true&w=majority";
