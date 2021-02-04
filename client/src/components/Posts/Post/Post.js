30748875a7f8f277bf869f2feed5d34e81fbeafe
Alex_Kelly <afkelly14@Gmail.com>
2021-02-03 22:00:04 -0500

    card styling

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHorizIcon';
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
-        <h1>Post</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">
+
+                </Typography>
+            </div>
+        </Card>
     )
 }
 
